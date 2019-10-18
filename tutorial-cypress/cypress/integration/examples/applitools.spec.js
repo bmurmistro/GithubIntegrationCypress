40e5d67b6d02c004cfc9429dfb810af13e31da26
Sophie Tagar <sophie24587@gmail.com>
2019-10-18 10:50:20 -0700

    Update applitools.spec.js

--- tutorial-cypress/cypress/integration/examples/applitools.spec.js
+++ tutorial-cypress/cypress/integration/examples/applitools.spec.js
@@ -1,6 +1,6 @@
 describe('Hello Cypress, This is Applitools', () => {
   it('works', () => {
-    cy.visit('https://applitools.com/helloworld?diff2');
+    cy.visit('https://applitools.com/helloworld?diff1');
     cy.eyesOpen({
       appName: 'Hello Cypress, This is Applitools!',
       testName: 'My first Cypress Test',
