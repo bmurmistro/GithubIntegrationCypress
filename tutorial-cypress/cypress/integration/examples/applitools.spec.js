63ab892c9efd75fd89ac6dd9c1282a4763dfa865
Brandon Murray <bmurmistro@gmail.com>
2020-08-11 21:50:47 -0600

    Remove devices and try catch

--- tutorial-cypress/cypress/integration/examples/applitools.spec.js
+++ tutorial-cypress/cypress/integration/examples/applitools.spec.js
@@ -1,17 +1,12 @@
 describe('Hello Cypress, This is Applitools', () => {
   it('works', () => {
-    try {
-      cy.visit('https://applitools.com/helloworld');
-      cy.eyesOpen({
-        appName: 'Hello Cypress, This is Applitools!',
-        testName: 'Hello World'
-      });
-      cy.eyesCheckWindow('Hello');
-      //cy.contains('Click me!').click();
-      cy.eyesClose();
-    }
-    catch(e) {
-      console.log(e);
-    }
+    cy.visit('https://applitools.com/helloworld');
+    cy.eyesOpen({
+      appName: 'Hello Cypress, This is Applitools!',
+      testName: 'Hello World'
+    });
+    cy.eyesCheckWindow('Hello');
+    //cy.contains('Click me!').click()
+    cy.eyesClose();
   });
 });
