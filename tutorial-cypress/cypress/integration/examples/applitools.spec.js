6bde8fc425de6de2e7aab4b5ec808daf034cca47
Brandon Murray <bmurmistro@gmail.com>
2020-08-11 21:24:08 -0600

    debug

--- tutorial-cypress/cypress/integration/examples/applitools.spec.js
+++ tutorial-cypress/cypress/integration/examples/applitools.spec.js
@@ -1,12 +1,17 @@
 describe('Hello Cypress, This is Applitools', () => {
   it('works', () => {
-    cy.visit('https://applitools.com/helloworld');
-    cy.eyesOpen({
-      appName: 'Hello Cypress, This is Applitools!',
-      testName: 'Hello World'
-    });
-    cy.eyesCheckWindow('Hello');
-    //cy.contains('Click me!').click()
-    cy.eyesClose();
+    try {
+      cy.visit('https://applitools.com/helloworld');
+      cy.eyesOpen({
+        appName: 'Hello Cypress, This is Applitools!',
+        testName: 'Hello World'
+      });
+      cy.eyesCheckWindow('Hello');
+      //cy.contains('Click me!').click()
+      cy.eyesClose();
+    }
+    catch(e) {
+      console.log(e);
+    }
   });
 });
