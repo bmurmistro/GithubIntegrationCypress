aefe3738bb423e3401fd87ef78b3fe8bf4e9995c
Brandon Murray <bmurmistro@gmail.com>
2020-08-12 09:09:55 -0600

    Use hello world

--- tutorial-cypress/cypress/integration/examples/applitools.spec.js
+++ tutorial-cypress/cypress/integration/examples/applitools.spec.js
@@ -3,7 +3,7 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   // failing the test
   return false
 });
-/*describe('Hello Cypress, This is Applitools', () => {
+describe('Hello Cypress, This is Applitools', () => {
   it('works', () => {
     cy.visit('https://applitools.com/helloworld');
     cy.eyesOpen({
@@ -14,18 +14,23 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     //cy.contains('Click me!').click()
     cy.eyesClose();
   });
-});*/
+});
 
+/*
 describe('Hello Cypress, This is AutoDesk Help', () => {
   it('displays', () => {
-    cy.visit('https://app.digital-help-prd.autodesk.com/universal-help/current/index.html?uhmParentUrl=https%3A%2F%2Fknowledge.autodesk.com%2Fcontact-support&amp;uhmParentDomain=https%3A%2F%2Fknowledge.autodesk.com&amp;uhmParentReferrer=&amp;language=en_US&amp;product=generic&amp;pageTitle=Contact%20Support%20%7C%20Autodesk%20Knowledge%20Network&amp;showFullPage=true&amp;containerId=uhm-widget&amp;analyticsLogging=false');
+    cy.visit('https://knowledge.autodesk.com/contact-support');
     cy.eyesOpen({
       appName: 'Hello Cypress, This is AutoDesk help!',
       testName: 'Question Mark'
     });
+    cy.wait(10000);
+    cy.contains('No, thanks').click();
+    cy.wait(10000);
 
     cy.eyesCheckWindow('Hello');
 
     cy.eyesClose();
   });
 });
+*/
