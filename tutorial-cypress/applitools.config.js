09deca0615e0c6688b79618deb0574f96239bf61
bmurmistro <bmurmistro@gmail.com>
2021-07-28 23:27:45 -0600

    Add env

--- tutorial-cypress/applitools.config.js
+++ tutorial-cypress/applitools.config.js
@@ -19,7 +19,7 @@ module.exports = {
     //{width: 1024, height: 768, name: 'edgechromium'},
     //{width: 800, height: 600, name: 'safari'} ,
     // Add mobile emulation devices in Portrait mode
-    //{deviceName: 'iPhone X', screenOrientation: 'portrait'},
+    {deviceName: 'iPhone X', screenOrientation: 'portrait'},
     //{deviceName: 'Pixel 2', screenOrientation: 'portrait'}
   ],
   // set batch name to the configuration
