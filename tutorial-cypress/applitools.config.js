ff650eee5bab285310058eef515fd5bb95c5401f
bmurmistro <bmurmistro@gmail.com>
2022-06-25 20:28:25 -0600

    test

--- tutorial-cypress/applitools.config.js
+++ tutorial-cypress/applitools.config.js
@@ -4,10 +4,10 @@ module.exports = {
   browser: [
     // Add browsers with different viewports
     {width: 1000, height: 660, name: 'chrome'},
-    {width: 700, height: 500, name: 'firefox'},
+    //{width: 700, height: 500, name: 'firefox'},
     //{width: 1600, height: 1200, name: 'ie11'},
     //{width: 1024, height: 768, name: 'edgechromium'},
-    {width: 800, height: 600, name: 'safari'} ,
+    //{width: 800, height: 600, name: 'safari'} ,
     // Add mobile emulation devices in Portrait mode
     //{deviceName: 'iPhone X', screenOrientation: 'portrait'},
     //{deviceName: 'Pixel 2', screenOrientation: 'portrait'}
