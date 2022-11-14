1bc2fd9187c89b34ff9e1563682288d7d3653768
bmurmistro <bmurmistro@gmail.com>
2022-11-14 11:57:28 -0700

    Gap

--- tutorial-cypress/cypress/integration/examples/applitools.spec.js
+++ tutorial-cypress/cypress/integration/examples/applitools.spec.js
@@ -7,7 +7,7 @@ describe('Hello Cypress, This is Applitools', () => {
   it('works', () => {
     cy.visit('https://applitools.com/helloworld/?diff1')
     cy.eyesOpen({
-      appName: 'Demo App TRowe',
+      appName: 'Demo App Gap',
       testName: 'Demo Hello World'
     });
     //cy.contains('Click me!').click();
