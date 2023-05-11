6ecf53423b8e5cc6f3a596683a125b70bc9454d4
bmurmistro <bmurmistro@gmail.com>
2023-05-11 15:12:39 -0600

    fix batchId

--- tutorial-cypress/applitools.config.js
+++ tutorial-cypress/applitools.config.js
@@ -1,15 +1,11 @@
 const { execSync } = require('child_process')
-function getHeadHash()
-  return execSync('git rev-parse --verify HEAD').toString();
-}
-
 module.exports = {
+  batchName: 'BRANDON',
   testConcurrency: 4,
-  batchId: getHeadHash(),
   browser: [
     // Add browsers with different viewports
     {width: 1000, height: 660, name: 'chrome'},
-    //{width: 700, height: 500, name: 'firefox'},
+    {width: 700, height: 500, name: 'firefox'},
     //{width: 1600, height: 1200, name: 'ie11'},
     //{width: 1024, height: 768, name: 'edgechromium'},
     //{width: 800, height: 600, name: 'safari'} ,
@@ -17,10 +13,6 @@ module.exports = {
     //{deviceName: 'iPhone X', screenOrientation: 'portrait'},
     //{deviceName: 'Pixel 2', screenOrientation: 'portrait'}
   ],
-  // set batch name to the configuration
-  //batchName: 'Demo',
-  //branchName: 'bmurmistro/GithubIntegrationCypress/master',
-  //serverUrl: 'https://testeyesapi.applitools.com',
-  //batchId: process.env.APPLITOOLS_BATCH_ID,
+  batchId: execSync('git rev-parse --verify HEAD').toString(),
   failCypressOnDiff: false
 }
