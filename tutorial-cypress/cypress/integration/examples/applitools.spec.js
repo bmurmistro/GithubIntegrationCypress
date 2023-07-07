012d33422339a1d7832aa6d9bb5905ec6276a9ce
bmurmistro <bmurmistro@gmail.com>
2023-07-07 15:37:48 -0600

    reset

--- tutorial-cypress/cypress/integration/examples/applitools.spec.js
+++ tutorial-cypress/cypress/integration/examples/applitools.spec.js
@@ -11,7 +11,7 @@ describe('Hello Cypress, This is Applitools', () => {
       testName: 'Demo Hello World'
     });
     //cy.contains('?diff2').click();
-    cy.contains('Click me!').click();
+    //cy.contains('Click me!').click();
     cy.eyesCheckWindow('Hello');
     cy.eyesClose();
   });
