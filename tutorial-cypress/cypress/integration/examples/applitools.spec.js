1b7bae00eae2e117e28eec8f5257ff485bdd8449
bmurmistro <bmurmistro@gmail.com>
2024-02-20 11:56:06 +0530

    Reset

--- tutorial-cypress/cypress/integration/examples/applitools.spec.js
+++ tutorial-cypress/cypress/integration/examples/applitools.spec.js
@@ -7,7 +7,7 @@ describe('Hello Cypress, This is Applitools', () => {
   it('works', () => {
     cy.visit('https://applitools.com/helloworld')
     cy.eyesOpen({
-      appName: 'Apple',
+      appName: 'Apple2',
       testName: 'Hello'
     });
     //cy.contains('?diff2').click();
