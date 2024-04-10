60eb0c2758fc3ed12ef1d312937b7e32c17f4982
bmurmistro <bmurmistro@gmail.com>
2024-04-10 07:53:19 -0600

    reset for everfi

--- tutorial-cypress/cypress/integration/examples/applitools.spec.js
+++ tutorial-cypress/cypress/integration/examples/applitools.spec.js
@@ -7,7 +7,7 @@ describe('Hello Cypress, This is Applitools', () => {
   it('works', () => {
     cy.visit('https://applitools.com/helloworld')
     cy.eyesOpen({
-      appName: 'Sonatype Github',
+      appName: 'Everfi Github',
       testName: 'Hello'
     });
     //cy.contains('?diff2').click();
