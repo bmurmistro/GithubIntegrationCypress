274e91584d9ef45adb537ceb4a6a0551d52ffd5b
Brandon Murray <bmurmistro@gmail.com>
2025-09-09 20:47:06 -0600

    Remove testganai

--- tutorial-cypress/cypress.config.js
+++ tutorial-cypress/cypress.config.js
@@ -35,13 +35,7 @@ module.exports = defineConfig({
         // You may want to clean this up later by importing these.
         setupNodeEvents(on, config) {
             return require('./cypress/plugins/index.js')(on, config)
-            testGenAICypressTasksSetup(on);
         },
         specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-    },
-    env: {
-        applitoolsTestGenAI: {
-            autohealDataFolders: ['cypress/testgenai/autoheal'],
-        }
     }
 })
